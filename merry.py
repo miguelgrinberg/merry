811b8887bbc07735883347ce8aeffe780ac592d0
MomoCow <taocheng@qnap.com>
2019-11-26 14:28:58 +0800

    support async _try, _except, _else and _finnally

--- merry.py
+++ merry.py
@@ -14,6 +14,15 @@ class _Namespace:
     pass
 
 
+def _wrap_async(fn):
+    async def _inner(*args, **kwargs):
+        if inspect.iscoroutinefunction(fn):
+            return await fn(*args, **kwargs)
+        else:
+            return fn(*args, **kwargs)
+    return _inner
+
+
 class Merry(object):
     def __init__(self, logger_name='merry', debug=False):
         self.__logger = logging.getLogger(logger_name)
@@ -26,58 +35,106 @@ class Merry(object):
         self.__finally = None
 
     def _try(self, f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            ret = None
-            try:
-                ret = f(*args, **kwargs)
+        if inspect.iscoroutinefunction(f):
+            @wraps(f)
+            async def async_wrapper(*args, **kwargs):
+                ret = None
+                try:
+                    ret = await f(*args, **kwargs)
 
-                # note that if the function returned something, the else clause
-                # will be skipped. This is a similar behavior to a normal
-                # try/except/else block.
-                if ret is not None:
-                    return ret
-            except Exception as e:
-                # find the best handler for this exception
-                handler = None
-                for c in self.__except.keys():
-                    if isinstance(e, c):
-                        if handler is None or issubclass(c, handler):
-                            handler = c
+                    # note that if the function returned something, the else clause
+                    # will be skipped. This is a similar behavior to a normal
+                    # try/except/else block.
+                    if ret is not None:
+                        return ret
+                except Exception as e:
+                    # find the best handler for this exception
+                    handler = None
+                    for c in self.__except.keys():
+                        if isinstance(e, c):
+                            if handler is None or issubclass(c, handler):
+                                handler = c
 
-                # if we don't have any handler, we let the exception bubble up
-                if handler is None:
-                    raise e
+                    # if we don't have any handler, we let the exception bubble up
+                    if handler is None:
+                        raise e
 
-                # log exception
-                self.__logger.exception('[merry] Exception caught')
+                    # log exception
+                    self.__logger.exception('[merry] Exception caught')
 
-                # if in debug mode, then bubble up to let a debugger handle
-                debug = self.__debug
-                if handler in self.__force_debug:
-                    debug = True
-                elif handler in self.__force_handle:
-                    debug = False
-                if debug:
-                    raise e
+                    # if in debug mode, then bubble up to let a debugger handle
+                    debug = self.__debug
+                    if handler in self.__force_debug:
+                        debug = True
+                    elif handler in self.__force_handle:
+                        debug = False
+                    if debug:
+                        raise e
 
-                # invoke handler
-                if len(getargspec(self.__except[handler])[0]) == 0:
+                    # invoke handler
+                    return await _wrap_async(self.__except[handler])()
+                else:
+                    # if we have an else handler, call it now
+                    if self.__else is not None:
+                        return await _wrap_async(self.__else)()
+                finally:
+                    # if we have a finally handler, call it now
+                    if self.__finally is not None:
+                        alt_ret = await _wrap_async(self.__finally)()
+                        if alt_ret is not None:
+                            ret = alt_ret
+                        return ret
+            return async_wrapper
+        else:
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                ret = None
+                try:
+                    ret = f(*args, **kwargs)
+
+                    # note that if the function returned something, the else clause
+                    # will be skipped. This is a similar behavior to a normal
+                    # try/except/else block.
+                    if ret is not None:
+                        return ret
+                except Exception as e:
+                    # find the best handler for this exception
+                    handler = None
+                    for c in self.__except.keys():
+                        if isinstance(e, c):
+                            if handler is None or issubclass(c, handler):
+                                handler = c
+
+                    # if we don't have any handler, we let the exception bubble up
+                    if handler is None:
+                        raise e
+
+                    # log exception
+                    self.__logger.exception('[merry] Exception caught')
+
+                    # if in debug mode, then bubble up to let a debugger handle
+                    debug = self.__debug
+                    if handler in self.__force_debug:
+                        debug = True
+                    elif handler in self.__force_handle:
+                        debug = False
+                    if debug:
+                        raise e
+
+                    # invoke handler
                     return self.__except[handler]()
                 else:
-                    return self.__except[handler](e)
-            else:
-                # if we have an else handler, call it now
-                if self.__else_ is not None:
-                    return self.__else_()
-            finally:
-                # if we have a finally handler, call it now
-                if self.__finally_ is not None:
-                    alt_ret = self.__finally_()
-                    if alt_ret is not None:
-                        ret = alt_ret
-                    return ret
-        return wrapper
+                    # if we have an else handler, call it now
+                    if self.__else is not None:
+                        return self.__else()
+                finally:
+                    # if we have a finally handler, call it now
+                    if self.__finally is not None:
+                        alt_ret = self.__finally()
+                        if alt_ret is not None:
+                            ret = alt_ret
+                        return ret
+            return wrapper
 
     def _except(self, *args, **kwargs):
         def decorator(f):
@@ -92,11 +149,11 @@ class Merry(object):
         return decorator
 
     def _else(self, f):
-        self.__else_ = f
+        self.__else = f
         return f
 
     def _finally(self, f):
-        self.__finally_ = f
+        self.__finally = f
         return f
 
     # namespace accessors
