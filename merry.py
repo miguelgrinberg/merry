417ad0f1092305b01e2a390cbeb0216f894d3a9e
MomoCow <taocheng@qnap.com>
2019-11-26 13:59:42 +0800

    make attributes of Merry private

--- merry.py
+++ merry.py
@@ -1,6 +1,6 @@
-from functools import wraps
 import inspect
 import logging
+from functools import wraps
 
 getargspec = None
 if getattr(inspect, 'getfullargspec', None):
@@ -16,14 +16,14 @@ class _Namespace:
 
 class Merry(object):
     def __init__(self, logger_name='merry', debug=False):
-        self.logger = logging.getLogger(logger_name)
-        self.g = _Namespace()
-        self.debug = debug
-        self.except_ = {}
-        self.force_debug = []
-        self.force_handle = []
-        self.else_ = None
-        self.finally_ = None
+        self.__logger = logging.getLogger(logger_name)
+        self.__g = _Namespace()
+        self.__debug = debug
+        self.__except = {}
+        self.__force_debug = []
+        self.__force_handle = []
+        self.__else = None
+        self.__finally = None
 
     def _try(self, f):
         @wraps(f)
@@ -40,7 +40,7 @@ class Merry(object):
             except Exception as e:
                 # find the best handler for this exception
                 handler = None
-                for c in self.except_.keys():
+                for c in self.__except.keys():
                     if isinstance(e, c):
                         if handler is None or issubclass(c, handler):
                             handler = c
@@ -50,30 +50,30 @@ class Merry(object):
                     raise e
 
                 # log exception
-                self.logger.exception('[merry] Exception caught')
+                self.__logger.exception('[merry] Exception caught')
 
                 # if in debug mode, then bubble up to let a debugger handle
-                debug = self.debug
-                if handler in self.force_debug:
+                debug = self.__debug
+                if handler in self.__force_debug:
                     debug = True
-                elif handler in self.force_handle:
+                elif handler in self.__force_handle:
                     debug = False
                 if debug:
                     raise e
 
                 # invoke handler
-                if len(getargspec(self.except_[handler])[0]) == 0:
-                    return self.except_[handler]()
+                if len(getargspec(self.__except[handler])[0]) == 0:
+                    return self.__except[handler]()
                 else:
-                    return self.except_[handler](e)
+                    return self.__except[handler](e)
             else:
                 # if we have an else handler, call it now
-                if self.else_ is not None:
-                    return self.else_()
+                if self.__else_ is not None:
+                    return self.__else_()
             finally:
                 # if we have a finally handler, call it now
-                if self.finally_ is not None:
-                    alt_ret = self.finally_()
+                if self.__finally_ is not None:
+                    alt_ret = self.__finally_()
                     if alt_ret is not None:
                         ret = alt_ret
                     return ret
@@ -82,19 +82,19 @@ class Merry(object):
     def _except(self, *args, **kwargs):
         def decorator(f):
             for e in args:
-                self.except_[e] = f
+                self.__except[e] = f
             d = kwargs.get('debug', None)
             if d:
-                self.force_debug.append(e)
+                self.__force_debug.append(e)
             elif d is not None:
-                self.force_handle.append(e)
+                self.__force_handle.append(e)
             return f
         return decorator
 
     def _else(self, f):
-        self.else_ = f
+        self.__else_ = f
         return f
 
     def _finally(self, f):
-        self.finally_ = f
+        self.__finally_ = f
         return f
